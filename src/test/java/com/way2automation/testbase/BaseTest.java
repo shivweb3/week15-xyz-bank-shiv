6375110f6e9a2f5b30145d9826a937f9bbc2fe71
shivweb3 <shiv200019@gmail.com>
2022-12-16 20:27:18 +0000

    week15-xyz-bank-shiv

---
+++ src/test/java/com/way2automation/testbase/BaseTest.java
@@ -0,0 +1,21 @@
+package com.way2automation.testbase;
+
+import com.way2automation.propertyreader.PropertyReader;
+import com.way2automation.utility.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest extends Utility {
+
+    String browser = PropertyReader.getInstance().getProperty("browser");
+
+    @BeforeMethod
+    public void setUp(){
+        selectBrowser(browser);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        closeBrowser();
+    }
+}
